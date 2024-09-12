a829be4930c4c8d4964ffdb34bb5e5c3fb891936
Adrián Iraizos Mendoza <86532545+airaizos@users.noreply.github.com>
2024-09-12 21:07:03 +0200

    Añadido CardValidator y CardValitador Tests

---
+++ Sources/CardValidator/CardValidator.swift
@@ -0,0 +1,67 @@
+//
+//  File.swift
+//
+//
+//  Created by Adrian Iraizos Mendoza on 9/9/24.
+//
+
+/// # Kata: Card Validator
+/// ## Descripción
+/// Te han asignado la tarea de escribir el código de un validador de los números de tarjetas de banco. Para la validación tienes que utilizar el algoritmo de Luhn. https://es.wikipedia.org/wiki/Algoritmo_de_Luhn
+/// https://abrir-empresa.com/el-algoritmo-de-luhn-una-herramienta-esencial-para-la-verificacion-de-numeros-de-tarjetas-de-credito/
+///
+/// Los datos que recibirás son:
+/// **Número de tarjeta**, 16 dígitos, divididos en 4 números de 4 dígitos
+///
+/// La función debe lanzar error cuando:
+///  El grupo de números no tiene 4 dígitos.
+///  El número sea negativo
+///
+///
+// Ejemplo 1:
+// Input:
+// 1234 5678 9101 1121
+//
+// Output:
+//  False
+// No es un número de tarjeta válido
+
+// Ejemplo 2:
+// Input:
+//  4549 6489 -1064 1121
+// Output:
+// CardError.negativo
+// Lanza el tipo de error CardError
+
+
+/// Ejecuta los tests `CardValidatorTests` y verifica que todos pasen
+/// Recuerda que puedes crear todas las funciones, extensiones, clases y structs que necesites.
+///
+
+import Foundation
+
+enum CardError: Error {
+    case negativo, formato
+}
+struct BankCard {
+    let group1: Int
+    let group2: Int
+    let group3: Int
+    let group4: Int
+    
+    init(_ group1: Int,_ group2: Int,_ group3: Int,_ group4: Int) {
+        self.group1 = group1
+        self.group2 = group2
+        self.group3 = group3
+        self.group4 = group4
+    }
+    
+}
+
+struct CardValidator {
+    func validate(bankCard: BankCard) throws -> Bool {
+        //Escribe aquí tu código.
+        
+        return false
+    }
+}
---
+++ Tests/CardValidatorTests.swift
@@ -0,0 +1,89 @@
+//
+//  CardValidatorTests.swift
+//
+//
+//  Created by Adrian Iraizos Mendoza on 9/9/24.
+//
+
+import XCTest
+
+@testable import KatasSwift
+final class CardValidatorTests: XCTestCase {
+    
+    var sut: CardValidator!
+    
+    override func setUpWithError() throws {
+        sut = CardValidator()
+    }
+    
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+    
+    func testShouldThrowNegativeError() throws {
+        for card in TestsCardNumbers.negative {
+            XCTAssertThrowsErrorEqual(try sut.validate(bankCard: card), CardError.negativo)
+        }
+    }
+    
+    func testShouldThrowFormatError() throws {
+        for card in TestsCardNumbers.incorrectFormat {
+            XCTAssertThrowsErrorEqual(try sut.validate(bankCard: card), CardError.formato)
+        }
+    }
+    
+    func testShouldBeValid() throws {
+        for card in TestsCardNumbers.validCards {
+            XCTAssertTrue(try sut.validate(bankCard: card))
+        }
+    }
+    func testShouldBeInvalid() throws {
+        for card in TestsCardNumbers.validCards {
+            XCTAssertFalse(try sut.validate(bankCard: card))
+        }
+    }
+}
+
+
+fileprivate struct TestsCardNumbers {
+    static let validCards = [
+        BankCard(4539,1488,0343,6467),
+        BankCard(4716,1234,5678,9012),
+        BankCard(4916,6512,3456,7890),
+        BankCard(5404,1234,5678,9010),
+        BankCard(5296,7890,1234,5678),
+        BankCard(5222,8765,4321,0987),
+        BankCard(4485,1234,5678,9101),
+        BankCard(4556,2345,6789,0123),
+        BankCard(5156,2345,6789,1234),
+        BankCard(2223,4567,8901,2345)
+    ]
+    
+    static let invalidCard = [
+        BankCard(1234,5678,9101,1121),
+        BankCard(8765,4321,1234,5670),
+        BankCard(0000,1111,2222,3333),
+        BankCard(9999,8888,7777,6666),
+        BankCard(5432,1098,7654,3210),
+        BankCard(1111,1111,1111,1111),
+        BankCard(1234,1234,1234,1234),
+        BankCard(9999,0000,8888,1111),
+        BankCard(5432,1234,5678,9012),
+        BankCard(0000,0000,0000,0000)
+    ]
+    
+    static let negative = [
+        BankCard(-1234,5678,9101,1121),
+        BankCard(-1234,-5678,9101,1121),
+        BankCard(-1234,5678,-9101,1121),
+        BankCard(-1234,5678,9101,-1121),
+    ]
+    static let incorrectFormat = [
+        BankCard(12345,67891,01,1121),
+        BankCard(12,345678,0,011121),
+        BankCard(12,3456,789101,1121),
+        BankCard(1234,5678,910,1121),
+    ]
+}
+
+
