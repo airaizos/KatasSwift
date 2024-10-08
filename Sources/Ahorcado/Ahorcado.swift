8db8b931472cd45b0ef5ee548931150ebbe0ee15
Adrián Iraizos Mendoza <86532545+airaizos@users.noreply.github.com>
2024-10-08 20:12:30 +0200

    listadoPalabras

--- Sources/Ahorcado/Ahorcado.swift
+++ Sources/Ahorcado/Ahorcado.swift
@@ -10,20 +10,20 @@
 ///
 /// El String debe devolver lo siguiente, dependiendo del caso:
 ///  - Si se ha alcanzado el número de fallos permitidos: *"Oh no! has sido ahorcado"*
-///  - Si la palabra objetivo contiene o no la letra: *"Letras adivinadas: \(letrasAdivinadas). Prueba con otra letra. Llevas \(fallos) fallos."*
-/// - Si ha adivinado la palabra: *"Felicidades has ganado: La palabra era \(letrasAdivinadas) y solo con \(fallos) fallo(s)"*
+///  - Si la palabra objetivo contiene o no la letra: *"Letras adivinadas: _ _ _ _ _. Prueba con otra letra. Llevas 0 fallos."*
+/// - Si ha adivinado la palabra: *"Felicidades has ganado: La palabra era _ _ _ _ _. y solo con 0 fallo(s)"*
 ///
 // Ejemplo 1:
 // Ahorcado(palabraObjetivo: "Mac")
 //
 // intentoCon(letra: "m")
-// "Letras adivinadas: M__. Prueba con otra letra. Llevas 0 fallos."
+// "Letras adivinadas: M _ _. Prueba con otra letra. Llevas 0 fallos."
 //
 // intentoCon(letra: "z")
-// "Letras adivinadas: M__. Prueba con otra letra. Llevas 1 fallos."
+// "Letras adivinadas: M _ _. Prueba con otra letra. Llevas 1 fallos."
 //
 // intentoCon(letra: "a")
-// "Letras adivinadas: MA_. Prueba con otra letra. Llevas 1 fallos."
+// "Letras adivinadas: M A _. Prueba con otra letra. Llevas 1 fallos."
 //
 // intentoCon(letra: "c")
 // "Felicidades has ganado: La palabra era "MAC" y solo con 1 fallo(s)"
@@ -34,19 +34,21 @@
 import Foundation
 
 final class Ahorcado {
-    let fallosPermitidos: Int
-    let palabraObjetivo: String
-    
-    private var letrasRestantes: [String]
-    private var letrasAdivinadas: [String] = []
-    private var fallos = 0
-    private var juegoPerdido: Bool = false
-    
-    init(fallosPermitidos: Int = 3, palabraObjetivo: String) {
-        self.fallosPermitidos = fallosPermitidos
-        self.palabraObjetivo = palabraObjetivo
-        self.letrasRestantes = palabraObjetivo.map { "\($0)" }
-    }
+        let fallosPermitidos: Int
+        let listadoPalabras: [String]
+        private let palabraObjetivo: String
+        private var letrasRestantes: [String]
+        private var letrasAdivinadas: [String] = []
+        private var fallos = 0
+        private var juegoPerdido: Bool = false
+        
+        init(fallosPermitidos: Int = 3, listadoPalabras: [String]) {
+            self.fallosPermitidos = fallosPermitidos
+            self.listadoPalabras = listadoPalabras
+            let randomIndex = Int.random(in: 0..<listadoPalabras.count)
+            self.palabraObjetivo = listadoPalabras[randomIndex]
+            self.letrasRestantes = palabraObjetivo.map { "\($0)" }
+        }
     
     func intentoCon(letra: Character) -> String {
         //Escribe tu código aqui
--- Tests/AhorcadoTests.swift
+++ Tests/AhorcadoTests.swift
@@ -7,29 +7,70 @@
 
 import XCTest
 
+@testable import KatasSwift
 final class AhorcadoTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    var sut: Ahorcado!
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func setUp() {
+        sut = Ahorcado(listadoPalabras: ["Swift"])
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    override func tearDown() {
+        sut = nil
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testShouldBeHaSidoAhorcado() {
+        let _ = sut.intentoCon(letra: "A")
+        let _ = sut.intentoCon(letra: "B")
+        let final = sut.intentoCon(letra: "C")
+        XCTAssertEqual(final, "Oh no! has sido ahorcado")
     }
-
+    
+    func testShouldIntentosCorrectos() {
+        let firstAttempt = sut.intentoCon(letra: "S")
+        XCTAssertEqual(firstAttempt, "Letras adivinadas: S _ _ _ _. Prueba con otra letra. Llevas 0 fallos.")
+        
+        let secondAttempt = sut.intentoCon(letra: "w")
+        XCTAssertEqual(secondAttempt, "Letras adivinadas: S W _ _ _. Prueba con otra letra. Llevas 0 fallos.")
+        
+        let thirdAttempt = sut.intentoCon(letra: "i")
+        XCTAssertEqual(thirdAttempt, "Letras adivinadas: S W I _ _. Prueba con otra letra. Llevas 0 fallos.")
+        
+        let fourthAttempt = sut.intentoCon(letra: "f")
+        XCTAssertEqual(fourthAttempt, "Letras adivinadas: S W I F _. Prueba con otra letra. Llevas 0 fallos.")
+    }
+    func testShouldIntentosIncorrectos() {
+        let firstAttempt = sut.intentoCon(letra: "X")
+        XCTAssertEqual(firstAttempt, "Letras adivinadas: _ _ _ _ _. Prueba con otra letra. Llevas 1 fallos.")
+        
+        let secondAttempt = sut.intentoCon(letra: "A")
+        XCTAssertEqual(secondAttempt, "Letras adivinadas: _ _ _ _ _. Prueba con otra letra. Llevas 2 fallos.")
+        
+        let thirdAttempt = sut.intentoCon(letra: "P")
+        XCTAssertEqual(thirdAttempt, "Oh no! has sido ahorcado")
+    }
+    
+    func testShouldIntentosIncorrectos2Correctos2() {
+        let firstAttempt = sut.intentoCon(letra: "X")
+        XCTAssertEqual(firstAttempt, "Letras adivinadas: _ _ _ _ _. Prueba con otra letra. Llevas 1 fallos.")
+        
+        let secondAttempt = sut.intentoCon(letra: "A")
+        XCTAssertEqual(secondAttempt, "Letras adivinadas: _ _ _ _ _. Prueba con otra letra. Llevas 2 fallos.")
+        
+        let thirdAttempt = sut.intentoCon(letra: "I")
+        XCTAssertEqual(thirdAttempt, "Letras adivinadas: _ _ I _ _. Prueba con otra letra. Llevas 2 fallos.")
+        
+        let fourthAttempt = sut.intentoCon(letra: "T")
+        XCTAssertEqual(fourthAttempt, "Letras adivinadas: _ _ I _ T. Prueba con otra letra. Llevas 2 fallos.")
+    }
+    
+    func testPalabraAcertada() {
+        let _ = sut.intentoCon(letra: "S")
+        let _ = sut.intentoCon(letra: "W")
+        let _ = sut.intentoCon(letra: "I")
+        let _ = sut.intentoCon(letra: "F")
+        let last = sut.intentoCon(letra: "T")
+        XCTAssertEqual(last, "Felicidades has ganado: La palabra era S W I F T y solo con 0 fallo(s)")
+    }
 }
